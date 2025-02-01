6d65717cdcaf62e7cb1f08cba75053c93c9498cd
tresorud <149726085+tresorud@users.noreply.github.com>
2025-02-01 08:15:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,25 +4,28 @@ let score = 0
 
 // Déclaration de la variable contenant le mot saisi par l'utilisateur
 // Le prompt sert à afficher une popup demandant à l'utilisateur de saisir un mot
-let motUtilisateur = prompt('Entrez le mot : ' + listeMots[0])
+for (var i = 0; i < 3;i++){
+let motUtilisateur = prompt('Entrez le mot : ' + listeMots[i])
 
 // Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
-if (motUtilisateur === listeMots[0]) {
+if (motUtilisateur === listeMots[i]) {
     score++
 }
-
-// Idem avec le second mot du tableau
-// Notez l'absence du "let" devant la déclaration de la variable "motUtilisateur", cela permet de réutiliser la variable déclarée précédemment
-motUtilisateur = prompt('Entrez le mot : ' + listeMots[1])
-if (motUtilisateur === listeMots[1]) {
-    score++
 }
+console.log(score)
 
-// Idem avec le troisième mot du tableau
-motUtilisateur = prompt('Entrez le mot : ' + listeMots[2])
-if (motUtilisateur === listeMots[2]) {
-    score++
+const listePhrases = ["Pas de panique !”, “La vie, l’univers et le reste”, “Merci pour le poisson”]
+let choix = ''
+
+while (choix !== 'mot' || choix !== 'phrase'){
+    choix = prompt('choisissez entre mot et phrase ')
 }
-
-// Affichage du score de l'utilisateur
-console.log("Votre score est de " + score + " sur 3")
+if (choix === 'mot'){
+    for(let i = 0;i < 3;i++){
+        console.log(listeMots[i])
+    }
+}else {
+    for(let i = 0;i < 3;i++){
+        console.log(listePhrases[i])
+    }
+}
